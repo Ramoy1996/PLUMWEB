77f6b77592d8ccfad79f715efc4a8df98b4d36f8
Ramoy Roberts <rroberts20j@vtdi.edu.jm>
2022-05-17 04:13:18 -0500

    email confrimation works!!!

--- PLUMWEB/Data/Admin.cs
+++ PLUMWEB/Data/Admin.cs
@@ -3,8 +3,6 @@ namespace PLUMWEB.Data
 {
     public class Admin : IdentityUser
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public DateTime DateJoined { get; set; }
+       
     }
 }
